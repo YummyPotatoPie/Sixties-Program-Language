d642a5ce25578dfbd0d5376766d62efb2f7af274
YummyPotatoPie <53563234+YummyPotatoPie@users.noreply.github.com>
2020-10-09 01:59:23 +0300

    Функции ввода-вывода

---
+++ InOut.cs
@@ -0,0 +1,28 @@
+﻿namespace SPL
+{
+    static class InOut
+    {
+        static public void Input(string Name)
+        {
+            if (Memory.IsInMemory(Name))
+            {
+                System.Console.Write("INPUT: ");
+                string Data = System.Console.ReadLine();
+                if (TokenBuilder.IsNumber(ref Data))
+                        Memory.SetData(Name, System.Convert.ToDouble(Data));
+                else
+                    Interpreter.ErrorFlag = "ERROR: INVALID INPUT";
+            }
+            else
+            {
+                Interpreter.ErrorFlag = "ERROR: UNKNOWN IDENTIFICATOR";
+            }
+        }
+
+        static public void Output(string Name)
+        {
+            if (Memory.IsInMemory(Name)) System.Console.WriteLine("OUTPUT: " + Memory.GetData(Name));
+            else Interpreter.ErrorFlag = "ERROR: UNKNOWN IDENTIFICATOR";
+        }
+    }
+}
