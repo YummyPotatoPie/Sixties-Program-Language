9ab2655692adc60004d4c49193ca86f4a76ad924
YummyPotatoPie <53563234+YummyPotatoPie@users.noreply.github.com>
2020-10-09 01:58:59 +0300

    Класс переменных

---
+++ Variable.cs
@@ -0,0 +1,16 @@
+﻿namespace SPL
+{
+    class Variable
+    {
+        public string Name  { get; private set; }
+        public double Value { get; private set; }
+
+        public void SetValue(double Value) => this.Value = Value;
+
+        public Variable(string Name, double Value)
+        {
+            this.Name = Name;
+            this.Value = Value;
+        }
+    }
+}
